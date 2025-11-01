9fcb40c6970854bf11f5f815bd25cd98012c7c7d
Qmaymay <13423767696@163.com>
2025-11-01 14:29:36 +0800

    测试Git推送更新版本

--- calculator/version.h
+++ calculator/version.h
@@ -5,11 +5,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-// 自动版本号 - 重新开始
+// C开发只管前两位，第三位永远为0
 #define CALC_MAJOR_VERSION 1
-#define CALC_MINOR_VERSION 11
-#define CALC_PATCH_VERSION 8
-#define CALC_VERSION "1.11.8"
+#define CALC_MINOR_VERSION 9
+#define CALC_VERSION "1.9.0"  // 第三位固定为0
+
 
 static inline const char* get_version(void) {
     return CALC_VERSION;

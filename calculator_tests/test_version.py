85189db27d088f31059274d994cc6a9e6667d0cf
Qmaymay <13423767696@163.com>
2025-11-01 19:37:37 +0800

    测试修复后的钩子

--- calculator_tests/test_version.py
+++ calculator_tests/test_version.py
@@ -1,10 +1,10 @@
 
-                C_MAJOR = 1
-                C_MINOR = 9
-                TEST_REVISION = 2
-                
-                TEST_VERSION = f"{C_MAJOR}.{C_MINOR}.{TEST_REVISION}"
-                
-                def get_test_version():
-                    return TEST_VERSION
-                
+C_MAJOR = 1
+C_MINOR = 11
+TEST_REVISION = 3
+
+TEST_VERSION = f"{C_MAJOR}.{C_MINOR}.{TEST_REVISION}"
+
+
+def get_test_version():
+    return TEST_VERSION

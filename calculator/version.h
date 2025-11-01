a89840195f5f578ce34f1528b94b9e491b4d014b
Qmaymay <13423767696@163.com>
2025-11-01 20:49:49 +0800

    自动化calculator和calculator_tests版本管理

--- calculator/version.h
+++ calculator/version.h
@@ -8,7 +8,7 @@
 // C开发只管前两位，第三位永远为0
 #define CALC_MAJOR_VERSION 1
 #define CALC_MINOR_VERSION 19
-#define CALC_VERSION "1.19.0"  // 第三位固定为0
+#define CALC_VERSION "1.8.0"  // 第三位固定为0
 
 
 static inline const char* get_version(void) {

5f0fb9ed032dace3dc6b0ef1efab862c8e1286cb
Qmaymay <13423767696@163.com>
2025-11-01 19:47:27 +0800

    更新测试用例

--- calculator_tests/test_version.py
+++ calculator_tests/test_version.py
@@ -1,10 +1,13 @@
+# 测试版本管理
+# 自动生成，请勿手动修改
 
 C_MAJOR = 1
 C_MINOR = 11
-TEST_REVISION = 6
+TEST_REVISION = 7
 
 TEST_VERSION = f"{C_MAJOR}.{C_MINOR}.{TEST_REVISION}"
 
 
 def get_test_version():
+    """获取当前测试版本号"""
     return TEST_VERSION
